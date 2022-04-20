a675b0ccd880b1c466bbf6685ac9325316eda9be
MoAI522 <urotanre522@gmail.com>
2022-04-20 10:59:20 +0900

    bubble

--- ricora-ta/src/main.ts
+++ ricora-ta/src/main.ts
@@ -1,3 +1,4 @@
+import bubble from "./components/bubble";
 import girl from "./components/girl";
 import "./style.css";
 
@@ -6,6 +7,7 @@ const preprocess = () => {};
 const onload = () => {
   window.scroll(0, 0);
   girl.init();
+  bubble.init();
 };
 
 preprocess();
